d5396b000a803dc067a9575fad6164c1a31d1217
texas2010 <texas2010@users.noreply.github.com>
2017-08-05 21:35:34 -0500

    navbar

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,25 @@ var config = {
   },
   "html": {
     "src": ["**/*.html"]
+  },
+  "css": {
+    "src": ["**/*.css"]
   }
 };
 
 var gulp         = require('gulp');
 var browserSync  = require('browser-sync');
 
+// CSS Task
+gulp.task('css', function() {
+  return gulp.src(config.working_path + '/' + config.css.src);
+});
+
 // Static Task
 gulp.task('static', function() {
   return browserSync({
     server: {
       baseDir: config.working_path
-      //directory: true
     },
     open: false
   });
@@ -25,7 +32,8 @@ gulp.task('static', function() {
 
 // Watch Task
 gulp.task('watch', function() {
-  gulp.watch(config.working_path + '/' + config.html.src).on("change", browserSync.reload)
+  gulp.watch(config.working_path + '/' + config.html.src).on("change", browserSync.reload);
+  gulp.watch(config.working_path + '/' + config.css.src, ['css', browserSync.reload]);
 });
 
 // Default Task
