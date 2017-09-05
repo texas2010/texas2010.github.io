e139aba640e047bf15e1910f89805590986ebcd6
texas2010 <texas2010@users.noreply.github.com>
2017-09-04 19:00:39 -0500

    Set up jade to the HTML in the Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,37 @@ var config = {
   },
   "css": {
     "src": ["**/*.css"]
+  },
+  "jade": {
+    "src": ["./index.jade"],
+    "dest": "./"
   }
 };
 
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync');
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var data =  require('gulp-data');
+var jade = require('gulp-jade');
 
 // CSS Task
-gulp.task('css', function() {
+gulp.task('css', function () {
   return gulp.src(config.baseDir + '/' + config.css.src);
 });
 
+// Jade Task
+gulp.task('jade', function () {
+  return gulp.src(config.jade.src)
+    .pipe(data(function(file) {
+      return require('./myconfig.json');
+    }))
+    .pipe(jade({
+      pretty: true,
+    }))
+    .pipe(gulp.dest(config.jade.dest));
+});
+
 // Static Task
-gulp.task('static', function() {
+gulp.task('static', function () {
   return browserSync({
     server: {
       baseDir: config.baseDir
@@ -31,10 +49,11 @@ gulp.task('static', function() {
 });
 
 // Watch Task
-gulp.task('watch', function() {
-  gulp.watch(config.baseDir + '/' + config.html.src).on("change", browserSync.reload);
+gulp.task('watch', function () {
+  // gulp.watch(config.baseDir + '/' + config.html.src).on("change", browserSync.reload);
   gulp.watch(config.baseDir + '/' + config.css.src, ['css', browserSync.reload]);
+  gulp.watch("./**/*.jade", ['jade', browserSync.reload]);
 });
 
 // Default Task
-gulp.task('default', [config.default.server, 'watch']);
+gulp.task('default', ['jade', config.default.server, 'watch']);
