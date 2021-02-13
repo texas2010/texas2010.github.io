cf4c5238a520c2fe80e3de3c7ff7f2d1ba84b2fb
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-02-13 00:06:21 -0600

    add hover css

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -12,10 +12,10 @@ export default class index extends Component {
             skills
         } = this.props.project
         return (
-            <Card style={{ flex: 'none' }} bg="dark" text="white" className="mb-4 project-card" border="primary">
+            <Card style={{ flex: 'none' }} bg="dark" text="white" className="mb-4 project-card hvr-sweep-to-right">
                 <a href={projectLink}>
                     <Card.Img variant="top" src={`/images/${imgLink}`} alt={title} />
-                    <Card.Body>
+                    <Card.Body className="">
                         <Card.Title className='text-white'>{title}</Card.Title>
                         <Card.Text className='text-white'>{body}</Card.Text>
                         <Card.Text className='text-white'>{skills}</Card.Text>
