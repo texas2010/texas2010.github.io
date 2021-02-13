dbf6b05a61c691a1b82347785b1a7737dd7c219b
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-02-13 00:56:58 -0600

    add copy symbol

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Container, Row } from 'react-bootstrap'
 import NavBar from '../NavBar'
 import AboutSection from '../AboutSection'
 import ProjectSection from '../ProjectSection'
@@ -12,6 +13,11 @@ export default class App extends Component {
                 <AboutSection />
                 <ProjectSection />
                 <ContactSection />
+                <Container className="mb-5">
+                    <Row>
+                        <p className="mx-auto text-white">&copy; {new Date().getFullYear()} texas2010</p>
+                    </Row>
+                </Container>
             </div>
         )
     }
