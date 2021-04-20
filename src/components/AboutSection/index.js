86105527bb0869990a2953a38dc650d0f6c02751
texas2010 <9701609+texas2010@users.noreply.github.com>
2021-04-19 23:44:53 -0500

    update bio summary

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,60 +1,70 @@
-import React, { Component } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import './style.css'
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./style.css";
 
 export default class index extends Component {
-    constructor() {
-        super()
-        this.state = {
-            aboutList: [
-                {
-                    title: 'Languages',
-                    body: ['HTML', 'CSS', 'JavaScript', 'PHP', 'MySQL', 'MongoDB']
-                },
-                {
-                    title: 'Libraries/Frameworks',
-                    body: ['Bootstrap', 'React', 'Node.js', 'Express.js', 'Mongoose']
-                },
-                {
-                    title: 'Tools',
-                    body: ['Visual Studio Code', 'Git', 'Terminal', 'Gulp.js', 'Webpack']
-                }
-            ]
-        }
-    }
-    render() {
-        const aboutList = this.state.aboutList.map(({ title, body }) => {
-            return (
-                <Col key={title}>
-                    <hr className="border d-md-none" />
-                    <p className="h4 text-color text-center">{title}</p>
-                    <div className="d-flex justify-content-center">
-                        <ul className="about-list">
-                            {body.map((item) => (<li key={item} className="text-color">{item}</li>))}
-                        </ul>
-                    </div>
-                </Col>
-            )
-        })
-        return (
-            <Container>
-                <Row className="justify-content-center mb-1">
-                    <Col>
-                        <h1 id="about" className="text-center text-primary">About</h1>
-                    </Col>
-                </Row>
-                <Row className="justify-content-center mb-2">
-                    <Col md={8}>
-                        <p className="text-center lead text-color">
-                            I am self-taught programmer who loves to teach programming whose aim is to become a front end or back end web developer.
-                    </p>
-                    </Col>
-                </Row>
-                <Row className="d-block d-md-flex justify-content-center">
-                    {aboutList}
-                </Row>
-                <hr className="border-primary border-bottom" />
-            </Container>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      aboutList: [
+        {
+          title: "Languages",
+          body: ["HTML", "CSS", "JavaScript", "PHP", "MySQL", "MongoDB"],
+        },
+        {
+          title: "Libraries/Frameworks",
+          body: ["Bootstrap", "React", "Node.js", "Express.js", "Mongoose"],
+        },
+        {
+          title: "Tools",
+          body: ["Visual Studio Code", "Git", "Terminal", "Gulp.js", "Webpack"],
+        },
+      ],
+    };
+  }
+  render() {
+    const aboutList = this.state.aboutList.map(({ title, body }) => {
+      return (
+        <Col key={title}>
+          <hr className="border d-md-none" />
+          <p className="h4 text-color text-center">{title}</p>
+          <div className="d-flex justify-content-center">
+            <ul className="about-list">
+              {body.map((item) => (
+                <li key={item} className="text-color">
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Col>
+      );
+    });
+    return (
+      <Container>
+        <Row className="justify-content-center mb-1">
+          <Col>
+            <h1 id="about" className="text-center text-primary">
+              About
+            </h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-center mb-2">
+          <Col md={8}>
+            <p className="text-center lead text-color">
+              I am a Front End Web Developer seeking a position that utilizes my
+              skills with React, among other JavaScript libraries. I'm always
+              looking for ways to innovate and help companies succeed by
+              creating user-friendly interfaces with a strong focus on features
+              and intuitive layouts.
+            </p>
+          </Col>
+        </Row>
+        <Row className="d-block d-md-flex justify-content-center">
+          {aboutList}
+        </Row>
+        <hr className="border-primary border-bottom" />
+      </Container>
+    );
+  }
 }
